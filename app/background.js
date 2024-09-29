e7b0b672a6c973fb02f7072d316377e70d1bc495
Adam Stiskala <adam@stiskala-yu.com>
2024-09-30 07:47:17 +0800

    Version 1.1

--- app/background.js
+++ app/background.js
@@ -1,68 +1,84 @@
-let detectedPSP = null;
+let detectedPSP = null
+let currentTabId = null
+let tabPsps = {}
 
+const eligibleUrls =
+  /^https:\/\/(?!chrome\.google\.com\/webstore|addons\.mozilla\.org|microsoftedge\.microsoft\.com\/addons)/
 const defaultIcons = {
-  "16": "images/default_16.png",
-  "48": "images/default_48.png",
-  "128": "images/default_128.png"
-};
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "detectPSP") {
-    detectedPSP = message.psp;
-
-    if (detectedPSP) {
-      fetch(chrome.runtime.getURL('psp-config.json'))
-        .then(response => response.json())
-        .then(pspConfig => {
-          const psp = pspConfig.psps.find(p => p.name === detectedPSP);
-          const icons = {
-            "16": `images/${psp.image}_16.png`,
-            "48": `images/${psp.image}_48.png`,
-            "128": `images/${psp.image}_128.png`
-          };
-
-          chrome.action.setIcon({ path: icons });
-        })
-        .catch(error => {
-          console.error("Error loading the JSON config:", error);
-        });
-    } else {
-      chrome.action.setIcon({ path: defaultIcons });
-    }
-
-  }
-
-  // When popup requests PSP data
-  if (message.action === "getPSP") {
-    sendResponse({ psp: detectedPSP });
-  }
-});
-
-function detectPSPOnActiveTab(tab) {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
-  });
+  16: 'images/default_16.png',
+  48: 'images/default_48.png',
+  128: 'images/default_128.png'
 }
 
-chrome.tabs.onActivated.addListener((tabInfo) => {
-  chrome.tabs.get(tabInfo.tabId, function (tab) {
-    if (/^https:\/\//.test(tab.url)) {
-      detectPSPOnActiveTab(tab);
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'detectPSP') {
+    detectedPSP = message.data.psp
+    if (detectedPSP) {
+      if (message.data.tabId == currentTabId) {
+        tabPsps[currentTabId] = detectedPSP
+        setPspIcon()
+      }
     } else {
-      chrome.action.setIcon({ path: defaultIcons });
+      chrome.action.setIcon({ path: defaultIcons })
     }
-  });
-});
+  }
+
+  if (message.action === 'getPSP') {
+    sendResponse({ psp: detectedPSP })
+  }
+
+  if (message.action === 'getTabId') {
+    sendResponse({ tabId: sender.tab.id })
+  }
+})
+
+chrome.tabs.onActivated.addListener(tabInfo => {
+  currentTabId = tabInfo.tabId
+  chrome.action.setIcon({ path: defaultIcons })
+  chrome.tabs.get(currentTabId, function (tab) {
+    // Run on any HTTPS website, excluding extension galleries
+    if (tab && eligibleUrls.test(tab.url)) {
+      detectedPSP = tabPsps[currentTabId]
+      setPspIcon()
+    }
+  })
+})
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    chrome.tabs.get(tabId, function (tab) {
-      if (/^https:\/\//.test(tab.url)) {
-        detectPSPOnActiveTab(tab);
-      } else {
-        chrome.action.setIcon({ path: defaultIcons });
-      }
-    });
+  if (changeInfo.status === 'loading') {
+    chrome.action.setIcon({ path: defaultIcons })
+    tabPsps[detectedPSP] = null
   }
-});
+
+  if (changeInfo.status === 'complete' && tab && tab.url) {
+    // Run on any HTTPS website, excluding extension galleries
+    if (eligibleUrls.test(tab.url)) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ['content.js']
+      })
+    } else {
+      chrome.action.setIcon({ path: defaultIcons })
+    }
+  }
+})
+
+function setPspIcon () {
+  fetch(chrome.runtime.getURL('psp-config.json'))
+    .then(response => response.json())
+    .then(pspConfig => {
+      const psp = pspConfig.psps.find(p => p.name === detectedPSP)
+      if (psp && psp.image) {
+        const icons = {
+          16: `images/${psp.image}_16.png`,
+          48: `images/${psp.image}_48.png`,
+          128: `images/${psp.image}_128.png`
+        }
+
+        chrome.action.setIcon({ path: icons })
+      }
+    })
+    .catch(error => {
+      console.error('Error loading the JSON config:', error)
+    })
+}
--- app/content.js
+++ app/content.js
@@ -1,25 +1,44 @@
-(() => {
-    fetch(chrome.runtime.getURL('psp-config.json'))
-        .then(response => response.json())
-        .then(pspConfig => {
-            const detectPSP = () => {
-                const pageContent = document.body.innerHTML;
+;(() => {
+  fetch(chrome.runtime.getURL('./psp-config.json'))
+    .then(response => response.json())
+    .then(pspConfig => {
+      const detectPSP = () => {
+        const pageContent = document.URL + '\n\n' + document.body.innerHTML
 
-                let detectedPSP = null;
-                for (let psp of pspConfig.psps) {
-                    const regex = new RegExp(psp.regex, 'i'); // Create a regex object
-                    if (regex.test(pageContent)) {
-                        detectedPSP = psp.name;
-                        break;
-                    }
-                }
+        let detectedPSP = null
+        for (let psp of pspConfig.psps) {
+          const regex = new RegExp(psp.regex, 'i') // Create a regex object
+          if (regex.test(pageContent)) {
+            detectedPSP = psp.name
+            break
+          }
+        }
 
-                chrome.runtime.sendMessage({ action: "detectPSP", psp: detectedPSP });
-            };
+        if (detectedPSP) {
+          chrome.runtime.sendMessage({ action: 'getTabId' }, response => {
+            let data = { psp: detectedPSP, tabId: response.tabId }
+            chrome.runtime.sendMessage({ action: 'detectPSP', data })
+          })
+        }
+      }
 
-            detectPSP();
-        })
-        .catch(error => {
-            console.error("Error loading the JSON config:", error);
-        });
-})();
+      detectPSP()
+
+      // Set up MutationObserver to listen for added elements
+      const observer = new MutationObserver(mutationsList => {
+        for (const mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+            if (mutation.addedNodes.length > 0) {
+              detectPSP()
+            }
+          }
+        }
+      })
+
+      const config = { childList: true, subtree: true }
+      observer.observe(document.body, config)
+    })
+    .catch(error => {
+      console.error('Error loading the JSON config:', error)
+    })
+})()
